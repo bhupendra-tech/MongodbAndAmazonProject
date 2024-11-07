c6512c711806bece60624410c386342bc01d271a
Bhupendra Vaishnav <yashvaishnavxyz@gmail.com>
2024-11-07 23:14:32 +0530

    added gradient animation on home page and created home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
-import Button from './components/button'
+import Navbar from './components/navbar'
 function App() {
-
-
   return (
-    <>
-     <Button type="secondary">Sign In</Button>
-     <Button type="primary">Sign Up</Button>
-     <Button type="text">Sign Up</Button>
-    </>
+    <div className='h-screen w-screen px-10 flex justify-between flex-col py-6 gradient-background'>
+     <Navbar/>
+     <div className='w-full' style={{"maxWidth" : 1100}}>
+     <h3 className='text-5xl'>A Intelligent system that will help you to learn things at your own pace.</h3>
+     <h6 className='text-xl mt-8 mb-14'> QUICK 
+      <span className='px-1 font-extrabold text-xl'> &#183; </span>
+          SMART 
+          <span className='px-1 font-extrabold text-xl'> &#183; </span> 
+        PERSONALIZED LEARNING
+        </h6>
+     </div>
+    </div>
   )
-}
+} 
 
 export default App
